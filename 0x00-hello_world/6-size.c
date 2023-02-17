8af40b6f16cace1ee9ae734a7e596456195bfef2
Eno-obong Vincent Ukpong <122833722+enovin@users.noreply.github.com>
2023-02-17 03:44:46 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Shows the size of various types ...
+ * ... on the computer that it is compiled and run on
  *
  * Return: Always 0 (Success)
  */
