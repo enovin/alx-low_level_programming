e83b0da9a334e2338cc7190fb09861d24105affb
Eno-obong Ukpong <enoobongvin@gmail.com>
2023-02-17 17:55:03 -0800

    Add 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
 
 	hex_digit = '0';
 
-	while (hex_digit >= '9')
+	while (hex_digit <= '9')
 	{
 		putchar(hex_digit);
 		hex_digit++;
@@ -21,7 +21,7 @@ int main(void)
 
 	hex_digit = 'a';
 
-	while (hex_digit >= 'f')
+	while (hex_digit <= 'f')
 	{
 		putchar(hex_digit);
 		hex_digit++;
