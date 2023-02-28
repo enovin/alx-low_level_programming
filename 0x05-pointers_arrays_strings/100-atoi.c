334421916dc8aeafb8c9a3260ba4e52e3fa40233
Eno-obong Ukpong <enoobongvin@gmail.com>
2023-02-27 16:16:10 -0800

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,68 +1,34 @@
 #include <limits.h>
 #include "main.h"
 
-/**
- * _atoi - Convert a string to an integer
+/** _atoi - Converts a string to an integer
+ * @s: The string to be converted
  *
- * @s: The string to convert
- *
- * Return: The integer value, or 0 if the string does not contain a valid integer
+ * Return: The converted integer
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int overflow = 0;
+        int res = 0;
+        int sign = 1;
+        int i = 0;
 
-    while (*s)
-    {
-        if (*s == '-')
+        while (s[i] != '\0')
         {
-            sign *= -1;
-        }
-        else if (*s >= '0' && *s <= '9')
-        {
-            int digit = *s - '0';
+                if (s[i] == '-')
+                        sign *= -1;
 
-            if (result > INT_MAX / 10)
-            {
-                overflow = 1;
-            }
-            else if (result == INT_MAX / 10 && digit > INT_MAX % 10)
-            {
-                overflow = 1;
-            }
-            else
-            {
-                result = result * 10 + digit;
-
-                if (result == INT_MIN)
+                if (s[i] >= '0' && s[i] <= '9')
                 {
-                    overflow = 1;
+                        if (res > INT_MAX / 10 || (res == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+                        {
+                                if (sign == 1)
+                                        return (INT_MAX);
+                                else
+                                        return (INT_MIN);
+                        }
+                        res = res * 10 + (s[i] - '0');
                 }
-                else if (result < INT_MIN)
-                {
-                    overflow = 1;
-                    result = 0;
-                }
-            }
+                i++;
         }
-        else if (*s != '+' || result != 0)
-        {
-            break;
-        }
-
-        s++;
-    }
-
-    if (overflow)
-    {
-        result = 0;
-    }
-    else
-    {
-        result *= sign;
-    }
-
-    return result;
+        return (sign * res);
 }
