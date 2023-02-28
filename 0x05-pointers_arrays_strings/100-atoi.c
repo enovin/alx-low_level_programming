d0534c86b5b02262eb12f4fff7bc1c0c87512aa6
Eno-obong Ukpong <enoobongvin@gmail.com>
2023-02-27 16:34:02 -0800

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,31 +2,34 @@
 
 /**
  * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
  *
- * @s: String to be converted.
- *
- * Return: The converted integer.
+ * Return: The integer value of the converted string.
  */
 int _atoi(char *s)
 {
-    int sign = 1, found_num = 0, num = 0;
+    int sign = 1;
+    unsigned int num = 0;
+    int started = 0;
 
-    while (*s != '\0')
+    while (*s)
     {
         if (*s == '-')
+        {
             sign *= -1;
+        }
         else if (*s >= '0' && *s <= '9')
         {
-            num *= 10;
-            num -= (*s - '0');
-            found_num = 1;
+            started = 1;
+            num = (num * 10) + (*s - '0');
         }
-        else if (found_num == 1)
+        else if (started)
+        {
             break;
+        }
+
         s++;
     }
 
-    num *= sign;
-
-    return (num);
+    return num * sign;
 }
