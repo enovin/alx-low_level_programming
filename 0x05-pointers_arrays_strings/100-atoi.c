be71090bd57e8d76cbcd0139fa92ba1cfbc6769a
Eno-obong Ukpong <enoobongvin@gmail.com>
2023-02-27 16:37:40 -0800

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,28 +8,28 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    unsigned int num = 0;
-    int started = 0;
+	int sign = 1;
+	unsigned int num = 0;
+	int started = 0;
 
-    while (*s)
-    {
-        if (*s == '-')
-        {
-            sign *= -1;
-        }
-        else if (*s >= '0' && *s <= '9')
-        {
-            started = 1;
-            num = (num * 10) + (*s - '0');
-        }
-        else if (started)
-        {
-            break;
-        }
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			started = 1;
+			num = (num * 10) + (*s - '0');
+		}
+		else if (started)
+		{
+			break;
+		}
 
-        s++;
-    }
+		s++;
+	}
 
-    return num * sign;
+	return num * sign;
 }
