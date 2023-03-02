343e34c4c0daba90c092ce3604f75dd2d8a88d63
Eno-obong Ukpong <enoobongvin@gmail.com>
2023-03-02 15:29:14 -0800

    Add 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,14 +8,14 @@
  * Return: An integer less than, equal to, or greater than zero
  * according to the lexicographical order of the strings
  */
-int _strcmp(char * s1, char * s2)
+int _strcmp(char *s1, char *s2)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
-    {
-        i++;
-    }
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	{
+		i++;
+	}
 
-    return (s1[i] - s2[i]);
+	return (s1[i] - s2[i]);
 }
